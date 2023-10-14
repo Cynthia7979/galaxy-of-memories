01e5b71a77d69f88f7e07e38ff9d1a6621779d32
Cynthia7979 <cynthiawang7979@hotmail.com>
2023-10-14 00:39:26 -0400

    fix: Back to the start

--- app/page.js
+++ app/page.js
@@ -2,60 +2,18 @@
 
 import { useState, useEffect } from 'react';
 import styles from './page.module.css'
-import { Canvas, camera, useThree } from "@react-three/fiber";
+import { Canvas } from "@react-three/fiber";
 import Star from './star.js';
-import { Vector3 } from 'three';
 
 export default function Home() {
-  const [MousePosition, setMousePosition] = useState({
-    x: 0,
-    y: 0
-  })
-
-  const [Camera, setCamera] = useState();
-
-  const size = useWindowSize();
-  // useThree(({ camera }) => setCamera(camera));
 
   return (
     <main className={styles.main}>
       <Canvas>
         <ambientLight />
-        <pointLight position={[MousePosition.x, MousePosition.y, 1]} decay={0} intensity={3}/>
+        <pointLight position={[0, 1, 1]} decay={0} intensity={3}/>
         <Star position={[0, 0, 0]}/>
       </Canvas>
     </main>
   )
-}
-
-function useWindowSize() {
-  // Initialize state with undefined width/height so server and client renders match
-  // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
-  const [windowSize, setWindowSize] = useState({
-    width: undefined,
-    height: undefined,
-  });
-
-  useEffect(() => {
-    // only execute all the code below in client side
-    // Handler to call on window resize
-    function handleResize() {
-      // Set window width/height to state
-      setWindowSize({
-        width: window.innerWidth,
-        height: window.innerHeight,
-      });
-    }
-    
-    // Add event listener
-    window.addEventListener("resize", handleResize);
-     
-    // Call handler right away so state gets updated with initial window size
-    handleResize();
-    
-    // Remove event listener on cleanup
-    return () => window.removeEventListener("resize", handleResize);
-  }, []); // Empty array ensures that effect is only run on mount
-
-  return windowSize;
 }
