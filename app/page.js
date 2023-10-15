1d9cfc86cf1c3c923ae3f3a29afccc8387da7d07
Cynthia7979 <cynthiawang7979@hotmail.com>
2023-10-14 21:40:25 -0400

    feat: Working zoom yay

--- app/page.js
+++ app/page.js
@@ -10,24 +10,18 @@ import * as THREE from 'three'
 import { Effects } from '@react-three/drei'
 import { UnrealBloomPass } from 'three-stdlib'
 import { OutputPass } from 'three/examples/jsm/postprocessing/OutputPass'
+import Controls from './controls';
 
 extend({ UnrealBloomPass, OutputPass })
 
 export default function Home() {
-  const [MousePosition, setMousePosition] = useState({
-    x: 0,
-    y: 0
-  })
-
   const { intensity, radius } = useState({
     intensity: { value: 0.34, min: 0, max: 1.5, step: 0.01 },
     radius: { value: 0.90, min: 0, max: 1, step: 0.01 }
   })
 
-  const [Camera, setCamera] = useState();
-
-  const size = useWindowSize();
-  // useThree(({ camera }) => setCamera(camera));
+  const [zoom, setZoom] = useState(false)
+  const [focus, setFocus] = useState({})
 
   return (
     <main className={styles.main}>
@@ -36,25 +30,25 @@ export default function Home() {
       </NextUIProvider>
       <Canvas>
         <ambientLight />
-        <pointLight position={[MousePosition.x, MousePosition.y, 1]} decay={0} intensity={3}/>
 
         <Effects disableGamma>
           {/* threshhold has to be 1, so nothing at all gets bloom by default */}
           <unrealBloomPass threshold={1} strength={0.34} radius={0.90} />
           <outputPass args={[THREE.ACESFilmicToneMapping]} />
         </Effects>
-        <Shape color="#fce6a6" position={[0, 0, 0]}>
+        <Shape color="#fce6a6" position={[0, 0, 0]} onZoom={(objectRef) => (setZoom(!zoom), setFocus(objectRef))} >
           <circleGeometry args={[0.8, 64]} />
         </Shape>
+        <Controls zoom={zoom} focus={focus} />
       </Canvas>
     </main>
   )
 }
 
-function Shape({ children, color, ...props }) {
+function Shape({ children, color, onZoom, ...props }) {
   const [hovered, hover] = useState(true)
   return (
-    <mesh {...props} onPointerOver={() => hover(false)} onPointerOut={() => hover(true)}>
+    <mesh {...props} onClick={(e) => onZoom(e.object.position)} onPointerOver={() => hover(false)} onPointerOut={() => hover(true)}>
       {children}
       {/* Now, in order to get selective bloom we simply crank colors out of
         their natural spectrum. Where colors are normally defined between 0 - 1 we push them
