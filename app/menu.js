4b7c286f6c4c428f8f4100c792bfca08cfe96d80
Cynthia7979 <cynthiawang7979@hotmail.com>
2023-10-15 03:52:41 -0400

    fix: Search bar

--- app/menu.js
+++ app/menu.js
@@ -6,7 +6,7 @@ import { symbol } from "prop-types";
 import { useState } from "react";
 import { useRef } from "react";
 
-export default function Menu({ searchCallback }) {
+export default function Menu({ searchCallback, messageCallback }) {
   const [query, setQuery] = useState("");
   const inputRef = useRef()
   const handleKeyUp = ev => {
@@ -22,8 +22,19 @@ export default function Menu({ searchCallback }) {
 
       <NavbarContent className={styles.search}>
         <Input
+          classNames={{
+            base: "max-w-full sm:max-w-[10rem] h-10",
+            mainWrapper: "h-full",
+            input: "text-small",
+            inputWrapper: "h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20",
+          }}
           placeholder="Search the Stars..."
-          startContent={<SearchIcon size={18} className={styles.icon}/>}
+          size="sm"
+          startContent={<SearchIcon size={18} />}
+          type="search"
+          ref={inputRef}
+          onKeyUp={handleKeyUp}
+          onChange={e => setQuery(e.currentTarget.value)}
         />
         <Dropdown>
           <DropdownTrigger>
@@ -38,7 +49,7 @@ export default function Menu({ searchCallback }) {
               </Button>
             </DropdownItem>
             <DropdownItem>
-              <Button className={styles.text2}>
+              <Button className={styles.text2} onClick={messageCallback}>
                 Impart a message
               </Button>
             </DropdownItem>
