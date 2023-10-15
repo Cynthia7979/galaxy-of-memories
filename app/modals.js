5096f86672fc6db16d0e25eebd2d2a47506c1867
Cynthia7979 <cynthiawang7979@hotmail.com>
2023-10-15 04:35:55 -0400

    feat: Defined modals

---
+++ app/modals.js
@@ -0,0 +1,32 @@
+import { Button } from "@nextui-org/react"
+import styles from "./modals.module.css"
+
+export function Modal({ children, visible, modalHeader, modalFooter, ...props}) {
+    return (
+        <div className={[styles.modal, visible ? styles.visible : styles.hidden].join(" ")} {...props}>
+            <div className={styles.modalHeader}>
+                {modalHeader}
+            </div>
+
+            <div className={styles.modalBody}>
+                {children}
+            </div>
+
+            <div className={styles.modalFooter}>
+                {modalFooter}
+            </div>
+        </div>
+    )
+}
+
+export function AddMessageModal ({ addMessageCallback, closeModal, ...props }) {
+    return (
+        <Modal
+            modalHeader={<>Impart a Message</>}
+            modalFooter={<Button onClick={closeModal}>Close</Button>}
+            {...props}
+        >
+            {"Hello :)"}
+        </Modal>
+    )
+}
