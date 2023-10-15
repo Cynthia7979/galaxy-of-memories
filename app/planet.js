dd91334f68316772c92e8e1095411ee9175dab6f
Cynthia7979 <cynthiawang7979@hotmail.com>
2023-10-15 03:10:33 -0400

    feat: Planet UI

--- app/planet.js
+++ app/planet.js
@@ -1,18 +1,48 @@
 import { Vector3 } from "three";
-import { useLayoutEffect } from "react";
+import { useLayoutEffect, useState } from "react";
 import { useRef } from "react";
 import { useMemo } from "react";
 import { useEffect } from "react";
+import { useCursor, Html } from "@react-three/drei";
+import styles from './planet.module.css'
+import { useFrame } from "@react-three/fiber";
 
 export default function Planet({ color, label, index, centerPos }) {
-    const radius = 0.8 * (index + 1);
+    const planetRef = useRef()
+    const [hovered, setHovered] = useState(false)
+    const [tOffset, setTOffset] = useState(0)
+    const [savedT, setSavedT] = useState(0)
+    const orbitRadius = 0.8 * (index + 1) + 0.5;
+    const planetPosition = [orbitRadius + centerPos[0], centerPos[1], centerPos[2]]
+    const planetRadius = 0.1
+    useCursor(hovered, 'help')
+
+    useFrame(({ clock }) => {
+        if (hovered) {
+            if (!savedT) setSavedT(clock.getElapsedTime())
+            if (savedT) setTOffset(clock.getElapsedTime() - savedT)
+        } else if (savedT) {
+            setSavedT(0)
+        }
+        const t = (clock.getElapsedTime() - tOffset) * orbitRadius;
+        const newPos = circularMotionAt(t, orbitRadius, centerPos)
+        planetRef.current.position.x = hovered ? planetRef.current.position.x : newPos.x
+        planetRef.current.position.y = hovered ? planetRef.current.position.y : newPos.y
+        planetRef.current.position.z = hovered ? planetRef.current.position.z : newPos.z
+    })
+
     return (
         <>
-            <mesh position={[radius + centerPos[0], centerPos[1], centerPos[2]]} >
-                <sphereGeometry args={[0.1, 32, 32]} />
-                <meshBasicMaterial color={color}/>
+            {hovered ? (
+            <Html center position={[planetRef.current.position.x + 1.5 * planetRadius, planetRef.current.position.y - 1.1 * planetRadius, planetRef.current.position.z]}>
+                <div className={styles.label}>{label}</div>
+            </Html>
+            ) : null}
+            <mesh position={planetPosition} onPointerOver={() => setHovered(true)} onPointerOut={() => setHovered(false)} ref={planetRef}>
+                <sphereGeometry args={[planetRadius, 32, 32]} />
+                <meshStandardMaterial color={color}/>
             </mesh>
-            <Orbit center={centerPos} radius={radius}/>
+            <Orbit center={centerPos} radius={orbitRadius}/>
         </>
     )
 }
@@ -21,17 +51,9 @@ const Orbit = ({ center = [0, 0, 0], radius = 1 }) => {
     const segments = 64
     const ref = useRef();
     const points = []
-    const tilt = Math.PI / 6
     for (let i = 0; i < segments; i++) {
         const angle = (i / segments) * 2 * Math.PI
-        const x = radius * Math.cos(angle)
-        const y = 0
-        const z = radius * Math.sin(angle)
-        const newX = x + center[0]
-        const newY = y * Math.cos(tilt) - z * Math.sin(tilt) + center[1]
-        const newZ = y * Math.sin(tilt) + z * Math.cos(tilt) + center[2]
-        
-        points.push(new Vector3(newX, newY, newZ))
+        points.push(circularMotionAt(angle, radius, center))
     }
     points.push(points[0])
     useEffect(() => {
@@ -43,4 +65,11 @@ const Orbit = ({ center = [0, 0, 0], radius = 1 }) => {
             <lineBasicMaterial attach="material" color="#ffffff" linewidth={10} />
         </line>
     )
+}
+
+const circularMotionAt = (t, radius, center, tilt=Math.PI/6) => {
+    const x = radius * Math.cos(t)
+    const y = 0
+    const z = radius * Math.sin(t)
+    return new Vector3(x + center[0], y * Math.cos(tilt) - z * Math.sin(tilt) + center[1], y * Math.sin(tilt) + z * Math.cos(tilt) + center[2])
 }
--- app/star.js
+++ app/star.js
@@ -46,6 +46,7 @@ export default function Star({ label: name, born, death, description, onClick_, 
 
       <Blooming {...props} color={color} position={position} onClick={(e) => onClick_()} hover={hover} setHover={setHover}>
         <sphereGeometry args={[starRadius]}/>
+        <pointLight color={color} intensity={10} decay={2} />
       </Blooming>
     </>
   );
@@ -59,7 +60,7 @@ function Blooming({ children, color, setZoom, hover, setHover, ...props }) {
         their natural spectrum. Where colors are normally defined between 0 - 1 we push them
         way out of range, into a higher defintion (HDR). What previously was [1, 1, 1] now could
         for instance be [10, 10, 10]. This requires that toneMapping is off, or it clamps to 1 */}
-      <meshStandardMaterial color={color} emissive={color} emissiveIntensity={hover ? 2 : 1} toneMapped={false} />
+      <meshStandardMaterial color={color} emissive={color} emissiveIntensity={hover ? 1.5 : 1} toneMapped={false} />
     </mesh>
   )
 }
